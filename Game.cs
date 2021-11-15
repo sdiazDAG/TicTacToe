d61c7171ff8f3f6c7e83a592f51032e69a6ab194
Santi Diaz <sdiaz@aidacanarias.com>
2021-11-15 22:28:11 +0000

    Implement IsThereWinner Method

--- Game.cs
+++ Game.cs
@@ -18,12 +18,47 @@ namespace TicTacToe
                 GameBoard.Cells[position.X, position.Y] = playerSymbol;
                 return GameBoard;
             }
+
             throw new NotFreePositionException();
         }
 
         public bool IsThereWinner(Board actualValue)
         {
-            return true;
+            for (int row = 0; row < 3; row++)
+            {
+                if (actualValue.Cells[row, 0] != ' ')
+                {
+                    if (actualValue.Cells[row, 0] == actualValue.Cells[row, 1] &&
+                        actualValue.Cells[row, 1] == actualValue.Cells[row, 2])
+                        return true;
+                }
+            }
+
+            for (int col = 0; col < 3; col++)
+            {
+                if (actualValue.Cells[0, col] != ' ')
+                {
+                    if (actualValue.Cells[0, col] == actualValue.Cells[1, col] &&
+                        actualValue.Cells[1, col] == actualValue.Cells[2, col])
+                        return true;
+                }
+            }
+
+            if (actualValue.Cells[0, 0] != ' ')
+            {
+                if (actualValue.Cells[0, 0] == actualValue.Cells[1, 1] &&
+                    actualValue.Cells[1, 1] == actualValue.Cells[2, 2])
+                    return true;
+            }
+
+            if (actualValue.Cells[0, 2] != ' ')
+            {
+                if (actualValue.Cells[0, 2] == actualValue.Cells[1, 1] &&
+                    actualValue.Cells[1, 1] == actualValue.Cells[2, 0])
+                    return true;
+            }
+
+            return false;
         }
     }
 }
