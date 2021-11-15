56f0bf3e7a024c00f68271d0fdf609c8f0c78f45
Santi Diaz <sdiaz@aidacanarias.com>
2021-11-15 22:43:39 +0000

    Refactoring Board class methods

--- Board.cs
+++ Board.cs
@@ -10,55 +10,39 @@ namespace TicTacToe
 
         public bool BoardDiagonalRightToLeftCompleted()
         {
-            if (Cells[0, 2] != ' ')
-            {
-                if (Cells[0, 2] == Cells[1, 1] &&
-                    Cells[1, 1] == Cells[2, 0])
-                    return true;
-            }
-
-            return false;
+            if (Cells[0, 2] == ' ') return false;
+            return Cells[0, 2] == Cells[1, 1] &&
+                   Cells[1, 1] == Cells[2, 0];
         }
 
         public bool BoardDiagonalLeftToRightCompleted()
         {
-            if (Cells[0, 0] != ' ')
-            {
-                if (Cells[0, 0] == Cells[1, 1] &&
-                    Cells[1, 1] == Cells[2, 2])
-                    return true;
-            }
-
-            return false;
+            if (Cells[0, 0] == ' ') return false;
+            return Cells[0, 0] == Cells[1, 1] &&
+                   Cells[1, 1] == Cells[2, 2];
         }
 
         public bool BoardColCompleted()
         {
-            for (int col = 0; col < 3; col++)
+            for (var col = 0; col < 3; col++)
             {
-                if (Cells[0, col] != ' ')
-                {
-                    if (Cells[0, col] == Cells[1, col] &&
-                        Cells[1, col] == Cells[2, col])
-                        return true;
-                }
+                if (Cells[0, col] == ' ') continue;
+                if (Cells[0, col] == Cells[1, col] &&
+                    Cells[1, col] == Cells[2, col])
+                    return true;
             }
-
             return false;
         }
 
         public bool BoardRowCompleted()
         {
-            for (int row = 0; row < 3; row++)
+            for (var row = 0; row < 3; row++)
             {
-                if (Cells[row, 0] != ' ')
-                {
-                    if (Cells[row, 0] == Cells[row, 1] &&
-                        Cells[row, 1] == Cells[row, 2])
-                        return true;
-                }
+                if (Cells[row, 0] == ' ') continue;
+                if (Cells[row, 0] == Cells[row, 1] &&
+                    Cells[row, 1] == Cells[row, 2])
+                    return true;
             }
-
             return false;
         }
     }
